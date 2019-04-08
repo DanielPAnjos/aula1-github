2a96511ee7e7cf63d2e88980d43b02737a753d5b
Daniel Pombo <pombo.daniel@gmail.com>
2019-04-08 11:10:29 -0300

    Acrescentada mensagem de bom dia

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
-package application;
+ package application;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Bom dia!");
 
 	}
 
